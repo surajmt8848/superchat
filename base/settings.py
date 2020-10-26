e3f2e49b281b0d97e5c277b13d2a876712957773
surajmt8848 <suraj8848mt@gmail.com>
2020-10-27 00:25:06 +0545

    config static settings

--- base/settings.py
+++ base/settings.py
@@ -136,4 +136,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'media',
+]
 STATIC_URL = '/static/'
+MEDIA_URL = '/media'
+
+STATIC_ROOT = BASE_DIR / 'static_cdn'
+MEDIA_ROOT = BASE_DIR / 'media_cdn'
+TEMP = BASE_DIR / 'media_cdn/temp'
+
+BASE_DIR = "http://127.0.0.1:8000"
+
+
