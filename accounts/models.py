7ac40a353657ccf3c4aeb9cd2e3c729a2cfec662
surajmt8848 <suraj8848mt@gmail.com>
2020-10-28 15:08:15 +0545

    created custom Manager

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,33 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+class AccountManager(BaseUserManager):
+
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not username:
+            raise ValueError("User must have username")
+        user = self.model(
+            email = self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username=username,
+            password=password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        return user
+
+
 def get_profile_image_filepath(self, filename):
     return f'profile_images/{self.pk}/{"profile_img.png"}'
 
@@ -28,17 +55,16 @@ class Account():
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+    objects = AccountManager()
+
     def __str__(self):
         return self.username
-    
+
     def get_profile_image_file_name(self):
         return str(self.profile_image)[str(self.profile_image).index('profile_images/{self.pk}/'):]
-    
+
     def has_perm(self, perm, obj=None):
         return self.is_admin
-    
+
     def has_module_perms(self, app_label):
         return True
-
-    
-
