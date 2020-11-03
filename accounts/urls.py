7d859e630012cfb29eb6ab6a6ea0ea1ec323823e
surajmt8848 <suraj8848mt@gmail.com>
2020-11-03 15:14:40 +0545

    fix template issues

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from accounts.views import (register_view)
+from accounts.views import (
+    register_view,
+    login_view,
+    logout_view
+)
 
 urlpatterns = [
-   path('register', register_view, name="register")
+    path('register', register_view, name="register"),
+    path('login', logout_view, name="login"),
+    path('logout', logout_view, name="logout")
+
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -66,5 +66,5 @@ def get_redirect_if_exists(request):
     redirect = None
     if request.GET:
         if request.GET.get("next"):
-            redirect str(request.GET.get("next"))
+            redirect = str(request.GET.get("next"))
     return redirect
